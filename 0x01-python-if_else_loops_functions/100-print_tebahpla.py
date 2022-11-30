b97e41ab551d1de7c1d9962e28a5e46497865fa8
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-11-30 02:02:01 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,4 @@
 for i in range(122, 96, -1):
     if i % 2 != 0:
         i = i - 32
-    print("{}".format(chr(i)), end="")#!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
     print("{}".format(chr(i)), end="")
